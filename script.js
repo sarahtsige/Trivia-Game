70af50e0a09202f187f7e5cce1866885c13355fb
sarahsaleh <sarahtsige@gmail.com>
2020-02-11 11:58:13 -0500

    created html, css, and js files

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log('hello!')
+
+let alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+'t', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+let word = "Hello"
+word.split(, 10);
+
