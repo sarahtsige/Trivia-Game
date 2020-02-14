80967ac960823aada8627c8310b222c7f1d11329
sarahsaleh <sarahtsige@gmail.com>
2020-02-13 22:08:39 -0500

    created question class and instances

--- script.js
+++ script.js
@@ -33,22 +33,22 @@ nextButton.addEventListener('click', nextQuestion)
 //if correct give player 50 points and display the next button
 //if not correct 
 
-function showQuestion(){
+function showQuestion() {
     //populate question from questions array
     question.innerText = questions[questionIndex].question;
     //answerButtons.forEach(element => element.innerText = sampleQuestion.answers[0])
-    answer1.innerHTML =  questions[questionIndex].answers[0].text;
-    answer2.innerHTML =  questions[questionIndex].answers[1].text;
-    answer3.innerHTML =  questions[questionIndex].answers[2].text;
-    answer4.innerHTML =  questions[questionIndex].answers[3].text;
+    answer1.innerHTML = questions[questionIndex].answers[0].text;
+    answer2.innerHTML = questions[questionIndex].answers[1].text;
+    answer3.innerHTML = questions[questionIndex].answers[2].text;
+    answer4.innerHTML = questions[questionIndex].answers[3].text;
 }
 
 function showFinalMessage() {
     let finalMessage = document.getElementById('final-message');
-    let correctAnswerCount =  document.getElementById('final-message__correct-answer-count');
+    let correctAnswerCount = document.getElementById('final-message__correct-answer-count');
     let finalScore = document.getElementById('final-message__final-score');
     finalScore.innerText = `Your final score is: ${score}`;
-    correctAnswerCount.innerText = `You correctly answered ${score/50} questions out of ${questions.length}`;
+    correctAnswerCount.innerText = `You correctly answered ${score / 50} questions out of ${questions.length}`;
     finalMessage.classList.toggle('hide');
 }
 
@@ -58,14 +58,14 @@ function nextQuestion() {
     if (questionIndex < questions.length) {
         showQuestion()
         reset()
-    } else  {
+    } else {
         nextButton.classList.add('hide');
         showFinalMessage();
     }
 }
 
-function reset(){
-    for (i=0; i < answerButtons.length; i++) {
+function reset() {
+    for (i = 0; i < answerButtons.length; i++) {
         answerButtons[i].style.backgroundColor = 'white';
     }
 }
@@ -73,17 +73,17 @@ function reset(){
 function checkAnswer(e) {
     console.log('answer clicked!')
     let clickedItem = e.target.id;
-     // take id of clicked item and check the correct key value (true or false) = 
-     if (questions[questionIndex].answers[clickedItem].correct == true){
-         console.log("That's Right");
-         e.target.style.backgroundColor = '#99ff99';
+    // take id of clicked item and check the correct key value (true or false) = 
+    if (questions[questionIndex].answers[clickedItem].correct == true) {
+        console.log("That's Right");
+        e.target.style.backgroundColor = '#99ff99';
         score += 50;
         scoreContainer.innerText = `Score: ${score}`;
         //if clicked item is the wrong answer, turn background red
-     } else {
-         console.log("sorry, that's incorrect")
-         e.target.style.backgroundColor = '#FF9999';
-     }
+    } else {
+        console.log("sorry, that's incorrect")
+        e.target.style.backgroundColor = '#FF9999';
+    }
     nextButton.classList.remove('hide');
     //if clicked button 
 }
@@ -112,11 +112,21 @@ function startGame() {
  * </p>
  */
 class Question {
-    constructor() {
-        this.question = "";
-        this.answer = "";
+    constructor(question, answers) {
+        this.question = question
+        this.answers = answers
     }
+
+    getAnswer() {
+        for (let i = 0; i < this.answers.length; i++) {
+            if (this.answers[i].correct == true)
+                return this.answers[i].text;
+        }
+    }
+
 }
+
+
 
 
 class Trivia {
@@ -129,10 +139,10 @@ class Trivia {
      * @param {int} x is the number of questions 
      */
     init(x) {
-        
+
     }
 
-    startGame () {
+    startGame() {
 
     }
 
@@ -144,8 +154,71 @@ class Trivia {
 
     }
 
-    nextQuestion () {
+    nextQuestion() {
 
+    }
+
+    generateQuestions(){
+        questions = [new Question("Virgin Trains, Virgin Atlantic and Virgin Racing, are all companies owned by which famous entrepreneur?", [
+            { text: "Richard Branson", correct: true, value: 50 },
+            { text: "Alan Sugar", correct: false, value: 0 },
+            { text: 'Donald Trump', correct: false, value: 0 },
+            { text: 'Bill Gates', correct: false, value: 0 }
+        ]),
+        new Question("Scotch whisky and Drambuie make up which cocktail?", [
+            { text: "Screwdriver", correct: false, value: 0 },
+            { text: "Manhattan", correct: false, value: 0 },
+            { text: "Rusty Nail", correct: true, value: 50 },
+            { text: "Sex on the Beach", correct: false, value: 0 }
+        ]),
+        new Question("Apple co-founder Steve Jobs died from complications of which form of cancer?", [
+            { text: "Bone", correct: false, value: 0 },
+            { text: "Liver", correct: false, value: 0 },
+            { text: "Stomach", correct: false, value: 0 },
+            { text: "Pancreatic", correct: true, value: 50 }
+        ]),
+        new Question("What was the original name of the search engine 'Google'?", [
+            { text: "Bone", correct: false, value: 0 },
+            { text: "Liver", correct: false, value: 0 },
+            { text: "Stomach", correct: false, value: 0 },
+            { text: "Pancreatic", correct: true, value: 50 }
+        ]),
+        new Question("What character was once considered to be the 27th letter of the alphabet?", [
+            { text: "Interrobang", correct: false, value: 0 },
+            { text: "Tilde", correct: false, value: 0 },
+            { text: "Pilcrow", correct: false, value: 0 },
+            { text: 'Ampersand', correct: true, value: 50 }
+        ]),
+        new Question("In what year was McDonald's founded?", [
+            { text: "1955", correct: true, value: 50 },
+            { text: "1964", correct: false, value: 0 },
+            { text: "1951", correct: false, value: 0 },
+            { text: "1947", correct: false, value: 0 }
+        ]),
+        new Question("What is the largest organ of the human body?", [
+            { text: "Heart", correct: false, value: 0 },
+            { text: "Skin", correct: true, value: 50 },
+            { text: "Large Intestine", correct: false, value: 0 },
+            { text: "Liver", correct: false, value: 0 }
+        ]),
+        new Question("What alcoholic drink is made from molasses?", [
+            { text: "Gin", correct: false, value: 0 },
+            { text: "Vodka", correct: false, value: 0 },
+            { text: "Rum", correct: true, value: 50 },
+            { text: "Whisky", correct: false, value: 0 }
+        ]),
+        new Question("How tall is the Burj Khalifa?", [
+            { text: "2,722 ft", correct: true, value: 50 },
+            { text: "2,717 ft", correct: false, value: 0 },
+            { text: "2,546 ft", correct: false, value: 0 },
+            { text: "3,024 ft", correct: false, value: 0 }
+        ]),
+        new Question("Which country, not including Japan, has the most people of japanese decent?", [
+            { text: "China", correct: false, value: 0 },
+            { text: "Brazil", correct: true, value: 50 },
+            { text: "South Korea", correct: false, value: 0 },
+            { text: "United States of America", correct: false, value: 0 }
+        ])]
     }
 
 }
@@ -155,92 +228,110 @@ class Trivia {
 
 
 
+// let q1 = new Question("Virgin Trains, Virgin Atlantic and Virgin Racing, are all companies owned by which famous entrepreneur?", [
+//     { text: "Richard Branson", correct: true, value: 50 },
+//     { text: "Alan Sugar", correct: false, value: 0 },
+//     { text: 'Donald Trump', correct: false, value: 0 },
+//     { text: 'Bill Gates', correct: false, value: 0 }
+// ])
+
 
 
 
 
 //Trivia question:
 let questions = [
-    {question: "Virgin Trains, Virgin Atlantic and Virgin Racing, are all companies owned by which famous entrepreneur?",
-    answers: [
-        {text:"Richard Branson", correct:true, value:50},
-        {text:"Alan Sugar", correct:false, value:0},
-        {text:'Donald Trump', correct:false, value:0},
-        {text:'Bill Gates', correct:false, value:0}
-    ]},
-    {question: "Scotch whisky and Drambuie make up which cocktail?",
-    answers: [
-        {text:"Screwdriver", correct:false, value:0},
-        {text:"Manhattan", correct:false, value:0},
-        {text:"Rusty Nail", correct:true, value:50},
-        {text:"Sex on the Beach", correct:false, value:0}
-    ]
-    },
-    {question: "Apple co-founder Steve Jobs died from complications of which form of cancer?",
-    answers: [
-        {text:"Bone", correct:false, value:0},
-        {text:"Liver", correct:false, value:0},
-        {text:"Stomach", correct:false, value:0},
-        {text:"Pancreatic", correct:true, value:50}
-    ]
-    },
-    {question: "What was the original name of the search engine 'Google'?",
-    answers: [
-        {text:"CatMassage", correct:false, value:0},
-        {text:"BackRub", correct:true, value:50},
-        {text:"SearchPro", correct:false, value:0},
-        {text:"Netscape Navigator", correct:false, value:0}
-    ]
-    },
-    {question: "What character was once considered to be the 27th letter of the alphabet?",
-    answers: [
-        {text:"Interrobang", correct:false, value:0},
-        {text:"Tilde", correct:false, value:0},
-        {text:"Pilcrow", correct:false, value:0},
-        {text:'Ampersand', correct:true, value:50}
-    ]
-    },
-    {question: "In what year was McDonald's founded?",
-    answers: [
-        {text:"1955", correct:true, value:50},
-        {text:"1964", correct:false, value:0},
-        {text:"1951", correct:false, value:0},
-        {text:"1947", correct:false, value:0}
-    ]
-    },
-    {question: "What is the largest organ of the human body?",
-    answers: [
-        {text:"Heart", correct:false, value:0},
-        {text:"Skin", correct:true, value:50},
-        {text:"Large Intestine", correct:false, value:0},
-        {text:"Liver", correct:false, value:0}
-    ]
-    },
-    {question: "What alcoholic drink is made from molasses?",
-    answers: [
-        {text:"Gin", correct:false, value:0},
-        {text:"Vodka", correct:false, value:0},
-        {text:"Rum", correct:true, value:50},
-        {text:"Whisky", correct:false, value:0}
-    ]
-    },
-    {question: "How tall is the Burj Khalifa?",
-    answers: [
-        {text:"2,722 ft", correct:true, value:50},
-        {text: "2,717 ft", correct:false, value:0},
-        {text:"2,546 ft", correct:false, value:0},
-        {text:"3,024 ft", correct:false, value:0}
-    ]
-    },
-    {question: "Which country, not including Japan, has the most people of japanese decent?",
-    answers: [
-        {text:"China", correct:false, value:0},
-        {text:"Brazil", correct:true, value:50},
-        {text:"South Korea", correct:false, value:0},
-        {text:"United States of America", correct:false, value:0}
-    ]
-    }
-]
+    // {
+    //     question: "Virgin Trains, Virgin Atlantic and Virgin Racing, are all companies owned by which famous entrepreneur?",
+    //     answers: [
+    //         { text: "Richard Branson", correct: true, value: 50 },
+    //         { text: "Alan Sugar", correct: false, value: 0 },
+    //         { text: 'Donald Trump', correct: false, value: 0 },
+    //         { text: 'Bill Gates', correct: false, value: 0 }
+    //     ]
+    // },
+    // {
+    //     question: "Scotch whisky and Drambuie make up which cocktail?",
+    //     answers: [
+    //         { text: "Screwdriver", correct: false, value: 0 },
+    //         { text: "Manhattan", correct: false, value: 0 },
+    //         { text: "Rusty Nail", correct: true, value: 50 },
+    //         { text: "Sex on the Beach", correct: false, value: 0 }
+    //     ]
+    // },
+    // {
+    //     question: "Apple co-founder Steve Jobs died from complications of which form of cancer?",
+    //     answers: [
+    //         { text: "Bone", correct: false, value: 0 },
+    //         { text: "Liver", correct: false, value: 0 },
+    //         { text: "Stomach", correct: false, value: 0 },
+    //         { text: "Pancreatic", correct: true, value: 50 }
+    //     ]
+    // },
+    // {
+    //     question: "What was the original name of the search engine 'Google'?",
+    //     answers: [
+    //         { text: "CatMassage", correct: false, value: 0 },
+    //         { text: "BackRub", correct: true, value: 50 },
+    //         { text: "SearchPro", correct: false, value: 0 },
+    //         { text: "Netscape Navigator", correct: false, value: 0 }
+    //     ]
+    // },
+    // {
+    //     question: "What character was once considered to be the 27th letter of the alphabet?",
+    //     answers: [
+    //         { text: "Interrobang", correct: false, value: 0 },
+    //         { text: "Tilde", correct: false, value: 0 },
+    //         { text: "Pilcrow", correct: false, value: 0 },
+    //         { text: 'Ampersand', correct: true, value: 50 }
+    //     ]
+    // // },
+    // {
+    //     question: "In what year was McDonald's founded?",
+    //     answers: [
+    //         { text: "1955", correct: true, value: 50 },
+    //         { text: "1964", correct: false, value: 0 },
+    //         { text: "1951", correct: false, value: 0 },
+    //         { text: "1947", correct: false, value: 0 }
+    //     ]
+    // },
+    // {
+    //     question: "What is the largest organ of the human body?",
+    //     answers: [
+    //         { text: "Heart", correct: false, value: 0 },
+    //         { text: "Skin", correct: true, value: 50 },
+    //         { text: "Large Intestine", correct: false, value: 0 },
+    //         { text: "Liver", correct: false, value: 0 }
+    //     ]
+//     // },
+//     {
+//         question: "What alcoholic drink is made from molasses?",
+//         answers: [
+//             { text: "Gin", correct: false, value: 0 },
+//             { text: "Vodka", correct: false, value: 0 },
+//             { text: "Rum", correct: true, value: 50 },
+//             { text: "Whisky", correct: false, value: 0 }
+//         ]
+//     },
+//     {
+//         question: "How tall is the Burj Khalifa?",
+//         answers: [
+//             { text: "2,722 ft", correct: true, value: 50 },
+//             { text: "2,717 ft", correct: false, value: 0 },
+//             { text: "2,546 ft", correct: false, value: 0 },
+//             { text: "3,024 ft", correct: false, value: 0 }
+//         ]
+//     },
+//     {
+//         question: "Which country, not including Japan, has the most people of japanese decent?",
+//         answers: [
+//             { text: "China", correct: false, value: 0 },
+//             { text: "Brazil", correct: true, value: 50 },
+//             { text: "South Korea", correct: false, value: 0 },
+//             { text: "United States of America", correct: false, value: 0 }
+//         ]
+//     }
+// ]
 
 
 
